a6b06692d7539fa5e8d74a57e5b8d40b51fdffe2
Robert-Nicolae Mocanu <robert.mocanu@integrisoft.ro>
2022-12-12 15:25:45 +0200

    replace old fe

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AccountsComponent } from './accounts/accounts.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    TransferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
