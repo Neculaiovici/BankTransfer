92549366abdc900b98ebfbdbfb210e638d45d7fc
INTEGRISOFT\robert.mocanu <robert.mocanu@integrisoft.ro>
2022-12-14 11:12:15 +0200

    adding services and interfaces, get

--- front-end/src/app/services.service.ts
+++ front-end/src/app/services.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Account } from './models/account.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
  //url api
-  private url = 'http://localhost:5273'
+  private url = 'https://localhost:7083/api/client'
   constructor(private http: HttpClient) { }
 
   //get accounts
-  
+  getAccount(): Observable<Account>{
+    return this.http.get<Account>(`${this.url}/robert@gmail.com`)
+  }
   //transfer money
   //error
 }
