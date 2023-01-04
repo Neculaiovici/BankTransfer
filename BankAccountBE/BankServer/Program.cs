3ce5878df9f611857b16a6e8c3dfab0999442e9f
INTEGRISOFT\robert.mocanu <robert.mocanu@integrisoft.ro>
2023-01-04 12:33:53 +0200

    adding dashboard grain to the BE

--- BankAccountBE/BankServer/Program.cs
+++ BankAccountBE/BankServer/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Hosting;
 using Orleans;
 using Orleans.Hosting;
+using Orleans.Statistics;
 using System.Net;
 
 await Host.CreateDefaultBuilder()
@@ -9,6 +10,16 @@ await Host.CreateDefaultBuilder()
         siloBuilder
             .UseLocalhostClustering(serviceId: "bank")
             .AddMemoryGrainStorageAsDefault()
+            .UseDashboard(options =>
+            {
+                options.Username = "admin";
+                options.Password = "admin";
+                options.Host = "*";
+                options.Port = 8080;
+                options.HostSelf = true;
+                options.CounterUpdateIntervalMs = 1000;
+            })
+            .UsePerfCounterEnvironmentStatistics()
             .UseTransactions();
     })
     .RunConsoleAsync();
